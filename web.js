0afb57d9113ca7e6ef2530d6868c682d140a57b4
Laguana <laguana@gmail.com>
2013-06-01 19:33:12 +1000

    some facebook integration hopefully

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require("express");
 var fs = require('fs');
 var app = express();
 var html = fs.readFileSync('index.html');
+var channel_html = '<script src="//connect.facebook.net/en_US/all.js"></script>';
 app.use(express.logger());
 
 app.get('/', function(request, response) {
 		response.setHeader("Content-Type", "text/html");
-		response.send(html);
+		if (request.params.name == 'channel.html'){ response.send(channel_html);}
+		else {response.send(html);}
 	});
 
 var port = process.env.PORT || 5000;
